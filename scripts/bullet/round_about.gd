fd76f998677225a701d2ba64ccf0b71dc2038780
Sina Majdieh <rhymewastaken@gmail.com>
2026-02-13 19:14:08 +0330

    added bullet animation and sprites round about bullet rotation removed because of sprite animations charged shoot removed from the range attack list (needs to be implemented with new animations and sprites)

--- scripts/bullet/round_about.gd
+++ scripts/bullet/round_about.gd
@@ -2,13 +2,9 @@ extends BaseBullet
 class_name RoundAboutBullet
 
 @export var max_speed: float = 800.0 
-@export var rotation_speed: float = 360.0 * 4 # Degrees per second
 @export var decel_time: float = 0.5 # Time in seconds to decelerate to a stop
 
 func _process(delta: float) -> void:
-	# Rotate the bullet around its center
-	rotation += deg_to_rad(rotation_speed * delta)
-
 	# Decelerate, stop, then accelerate backwards
 	speed = move_toward(speed, -max_speed, (max_speed / decel_time) * delta)
 
